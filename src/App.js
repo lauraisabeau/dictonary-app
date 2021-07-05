fe0bf29c2afe9ed09dc6e8b4d72a0565562a592a
lauraisabeau <79017668+lauraisabeau@users.noreply.github.com>
2021-07-05 15:05:42 +0200

    added the bootstrap sytlesheet and a footer with the link to netlify and github

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import SearchEngine from "./SearchEngine";
 function App() {
   return (
     <div className="App">
-      <h1>What are you looking for?</h1>
-      <SearchEngine />
+      <div>
+        <h1>What are you looking for?</h1>
+        <SearchEngine />
+      </div>
+      <footer>
+        coded by Laura Isabeau - open sourced on{" "}
+        <a href="https://github.com/lauraisabeau/dictonary-app" target="blank">
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://romantic-lamport-84df4e.netlify.app/" target="blank">
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
