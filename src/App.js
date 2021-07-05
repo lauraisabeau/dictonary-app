af828b0c00b6161c065bc7e17095a589f87d275e
lauraisabeau <79017668+lauraisabeau@users.noreply.github.com>
2021-07-05 15:02:11 +0200

    added a search engine component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import SearchEngine from "./SearchEngine";
 
 function App() {
   return (
     <div className="App">
       <h1>What are you looking for?</h1>
+      <SearchEngine />
     </div>
   );
 }
