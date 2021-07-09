d4852f6292b792ba7885e132a7944b7f03a5017e
lauraisabeau <79017668+lauraisabeau@users.noreply.github.com>
2021-07-09 12:58:45 +0200

    added the map function

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
-      <p>{props.meaning.definitions[0].definition}</p>
-      <p>
-        Here is an example: <em>{props.meaning.definitions[0].example}</em>
-      </p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+            <p>
+              Here is an example: <em>{definition.example}</em>
+            </p>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
     </div>
   );
 }
